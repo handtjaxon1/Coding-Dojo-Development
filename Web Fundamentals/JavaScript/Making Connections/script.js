4dd6fc1e1e6e3f519cc02131bea8cf0c8f5b8fce
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2021-11-13 16:10:12 -0600

    Added comments to the making connections JS to better explain it

--- Web Fundamentals/JavaScript/Making Connections/script.js
+++ Web Fundamentals/JavaScript/Making Connections/script.js
@@ -7,15 +7,18 @@ function editProfile() {
 function updateConnections(element) {
     // Uses the alt attribute value to determine if it was an accept button. Obviously a class would have worked good, but I thougt this was interesting to try out with attributes on elements.
     if (element.getAttribute("alt") === "accept") {
+        // If the "accept" image was clicked then add 1 to the total connections the user has
         updateConnectionAmount("#total-connections", 1);
     }
+    // Regardless of the image that's clicked, decrease the pending requests and remove the request
     updateConnectionAmount("#pending-connections", -1);
     removeConnectionRequest(element);
 }
 
+// Updates the text of one of the connection amounts (either total connections or pending connections)
 function updateConnectionAmount(id, amount) {
     var element = document.querySelector(id);
-    var newAmount = parseInt(element.textContent, 10);
+    var newAmount = parseInt(element.textContent, 10); // Converts the text to a number so we can add/subtract to it
     newAmount = newAmount + amount;
     element.textContent = newAmount;
 }
@@ -25,6 +28,7 @@ function removeConnectionRequest(icon) {
     icon.closest(".card-list-item").remove();
 }
 
+// Get all the buttons icons and programmatically add onclick events to remove the element once clicked
 var icons = document.querySelectorAll(".icon");
 icons.forEach(icon => {
     icon.addEventListener("click", function(){ updateConnections(icon); })
