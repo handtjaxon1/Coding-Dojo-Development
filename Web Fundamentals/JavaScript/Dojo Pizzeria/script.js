8ecbfbd7076882cb205c60713e9db7035f2c75b8
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2021-11-14 17:00:25 -0600

    Implemented the random pizza creation

--- Web Fundamentals/JavaScript/Dojo Pizzeria/script.js
+++ Web Fundamentals/JavaScript/Dojo Pizzeria/script.js
@@ -8,6 +8,62 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
 }
 
 var twoToppingPizza = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
-var grossPizza = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushroom", "olives", "onions"]);
-var hawaiianPizza = pizzaOven("thin crust", "marinara", ["mozzarella"], ["canadian bacon", "pineapple"]);
+var threeToppingPizza = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushroom", "olives", "onions"]);
+var hawaiianPizza = pizzaOven("thin", "marinara", ["mozzarella"], ["canadian bacon", "pineapple"]);
 var risingPizza = pizzaOven("rising", "marinara", ["mozzarella", "extra cheese"], ["pepperoni"]);
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin",
+    "rising"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "alfredo"
+];
+
+var cheeses = [
+    "mozzarella",
+    "parmesan",
+    "feta",
+    "extra cheese" // double the first choice of cheese
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "canadian bacon",
+    "pineapple",
+    "mushroom",
+    "olives",
+    "onions"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var index = Math.floor(arr.length * Math.random());
+    return arr[index];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for (let index = 0; index < randomRange(5, 1); index++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for (let index = 0; index < randomRange(5, 0); index++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
