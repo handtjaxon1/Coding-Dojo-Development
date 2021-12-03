5751b03e4af2e51cb8be40e81192e071cd726950
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2021-12-02 18:27:03 -0600

    Updated user class to allow function chaining

--- Python/Fundamentals/OOP/user.py
+++ Python/Fundamentals/OOP/user.py
@@ -1,29 +1,40 @@
 class User:
     # class attributes are defined in the class
     bank_name = "First National Dojo"
+    
     def __init__(self, name, email_address):
         # instance attributes are defined within the "constructor"
         self.name = name
         self.email_address = email_address
         self.account_balance = 0
+    
     # called to add to the user's account balance
     def makeDeposit(self, amount):
         self.account_balance += amount
+        print(f"Deposited ${amount} into {self.name}'s account.")
+        return self
+    
     # called to remove money from the user's account
     def makeWithdrawal(self, amount):
-        if self.account_balance - amount < 0:
-            print(f"Can't withdraw {amount} due to insufficient funds.")
-            return False
-        else:
-            self.account_balance -= amount
-            return True
+        self.account_balance -= amount
+        print(f"Withdrew ${amount} from {self.name}'s account.")
+        return self
+
+    # returns whether a withdrawal can be made
+    def canWithdraw(self, amount):
+        return self.account_balance - amount >= 0
+    
     # displays the account user and balance
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
+        return self
     
+    # transfer money from this user to another
     def transfer_user_money(self, other_user, amount):
-        if self.makeWithdrawal(amount):
-            other_user.makeDeposit(amount)
+        self.makeWithdrawal(amount)
+        other_user.makeDeposit(amount)
+        print(f"Transferred ${amount} from {self.name} to {other_user.name}")
+        return self
 
 # Create users
 michael = User("Michael Scott", "scottstots@aol.com")
