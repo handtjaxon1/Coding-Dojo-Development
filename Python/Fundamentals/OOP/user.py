f728a34c4f0336de4e982df313d7c335d1b44ea2
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2021-12-02 20:23:45 -0600

    Implemented multiple bank accounts per user

--- Python/Fundamentals/OOP/user.py
+++ Python/Fundamentals/OOP/user.py
@@ -1,4 +1,4 @@
-import bankAccount
+from bankAccount import BankAccount
 
 class User:
     # class attributes are defined in the class
@@ -8,47 +8,72 @@ class User:
         # instance attributes are defined within the "constructor"
         self.name = name
         self.email_address = email_address
-        self.account = bankAccount.BankAccount(0, 0.02)
+        # optional multiple accounts
+        self.accounts = {
+            "Primary": BankAccount(0, 0.2)
+        }
     
-    # called to add to the user's account balance
-    def makeDeposit(self, amount):
-        self.account.deposit(amount)
-        #print(f"Deposited ${amount} into {self.name}'s account.")
+    # Deposits money into one of the user's accounts
+    def makeDeposit(self, amount, account = "Primary"):
+        self.accounts[account].deposit(amount)
         return self
     
-    # called to remove money from the user's account
-    def makeWithdrawal(self, amount):
-        self.account.withdraw(amount)
-        #print(f"Withdrew ${amount} from {self.name}'s account.")
+    # Withdraws money from one of the user's accounts
+    def makeWithdrawal(self, amount, account = "Primary"):
+        self.accounts[account].withdraw(amount)
         return self
     
-    # displays the account user and balance
-    def display_user_balance(self):
-        print(f"User: {self.name}, Balance: ${self.account.balance}")
+    # Displays the total account balances for the user
+    def displayAllBalances(self):
+        print(f"User: {self.name}")
+        sum = 0
+        for account in self.accounts:
+            sum += self.accounts[account].balance
+            print(f"\tAccount: {account} \tBalance: ${self.accounts[account].balance}")
+        print(f"\t\t\t\tTotal: ${sum}")
         return self
     
-    # transfer money from this user to another
-    def transfer_user_money(self, other_user, amount):
-        self.account.withdraw(amount)
-        other_user.account.deposit(amount)
-        #print(f"Transferred ${amount} from {self.name} to {other_user.name}")
+    # Transfer money from this user from a specific account to another user's specificed account
+    def transferFunds(self, otherUser, amount, selfAccount = "Primary", otherAccount = "Primary"):
+        self.makeWithdrawal(amount, selfAccount)
+        otherUser.makeDeposit(amount, otherAccount)
+        return self
+
+    # Opens a new account with a give name
+    def openAccount(self, name, amount, int_rate):
+        self.accounts[name] = BankAccount(amount, int_rate)
         return self
 
 # Create users
 michael = User("Michael Scott", "scottstots@aol.com")
-kevin = User("Kevin Malone", "phoneymaloney@yahoo.com")
+jim = User("Jim Halpert", "phoneymaloney@yahoo.com")
 pam = User("Pam Beesly", "pamalabeesington@dundermifflin.com")
 
 # Michael deposits/withdrawals
-michael.makeDeposit(500).makeDeposit(200).makeDeposit(95).makeWithdrawal(170).display_user_balance()
+michael.makeDeposit(500).makeDeposit(200).makeDeposit(95).makeWithdrawal(170).displayAllBalances()
 
-# Kevin deposits/withdrawals
-kevin.makeDeposit(1000).makeDeposit(27).makeWithdrawal(550).makeWithdrawal(200).display_user_balance()
+# Jim deposits/withdrawals
+jim.makeDeposit(1000).makeDeposit(27).makeWithdrawal(550).makeWithdrawal(200).displayAllBalances()
 
 # Pam deposits/withdrawals
-pam.makeDeposit(3000).makeWithdrawal(750).makeWithdrawal(750).makeWithdrawal(750).display_user_balance()
+pam.makeDeposit(3000).makeWithdrawal(750).makeWithdrawal(750).makeWithdrawal(750).displayAllBalances()
 
 # Transfer money from Michael to Pam
-michael.transfer_user_money(pam,50)
-michael.display_user_balance()
-pam.display_user_balance()
+michael.transferFunds(pam, 50)
+michael.displayAllBalances()
+pam.displayAllBalances()
+
+# Create new accounts
+michael.openAccount("Investments", 1000, 0.08)
+jim.openAccount("Fun money", 69, 0.01)
+
+michael.displayAllBalances()
+jim.displayAllBalances()
+
+# Transferring money between other accounts
+michael.transferFunds(michael, 100, "Primary", "Investments")
+jim.transferFunds(pam, 69, "Fun money", "Primary")
+
+michael.displayAllBalances()
+jim.displayAllBalances()
+pam.displayAllBalances()
