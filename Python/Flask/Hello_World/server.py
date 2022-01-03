65a01c8a3f74d1dbaf3fc319a72c51137e20e2b5
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-03 17:52:39 -0600

    Updated the hello world server with new routes

--- Python/Flask/Hello_World/server.py
+++ Python/Flask/Hello_World/server.py
@@ -3,5 +3,10 @@ app = Flask(__name__)       # Create a new instance of the Flask class called "a
 @app.route('/')             # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'   # Return the string 'Hello World!' as a response
+
+@app.route("/success")
+def success():
+    return "Great Success!"
+
 if __name__=="__main__":    # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)     # Run the app in debug mode.
