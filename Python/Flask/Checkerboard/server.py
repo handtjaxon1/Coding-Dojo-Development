5521baeb46d073c7fc7661dcde69c98fde770594
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-04 20:45:54 -0600

    Implemented the checkerboard assignment requirements

--- Python/Flask/Checkerboard/server.py
+++ Python/Flask/Checkerboard/server.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", rows=8, cols=8)
+
+@app.route('/<int:rows>')
+def checkerboard(rows):
+    return render_template("index.html", rows=rows, cols=8)
+
+@app.route('/<int:rows>/<int:cols>')
+def checkerboard_xy(rows, cols):
+    return render_template("index.html", rows=rows, cols=cols)
+
+@app.route('/<int:rows>/<int:cols>/<color1>/<color2>')
+def checkboard_xy_colors(rows, cols, color1, color2):
+    return render_template("index.html", rows=rows, cols=cols, color1=color1, color2=color2)
 
 # only run if executing this file specifically
 if __name__=="__main__":
