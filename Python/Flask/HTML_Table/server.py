272de17928ad9f19496aa81b6bba009829b052ac
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-05 21:13:41 -0600

    Implemented users and added boostrap to HTML Table

--- Python/Flask/HTML_Table/server.py
+++ Python/Flask/HTML_Table/server.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("index.html")
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'},
+        {'first_name' : 'Jaxon', 'last_name' : 'Handt'}
+    ]
+    return render_template("index.html", users=users)
 
 # only run if executing this file specifically
 if __name__=="__main__":
