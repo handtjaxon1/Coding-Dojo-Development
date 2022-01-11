cbcdfaefcc1667a8aa4a56359ec64ed71f4b2427
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-10 18:05:16 -0600

    Completed the Dojo Fruit store assignment and requirements

--- Python/Flask/Dojo_Fruit_Store/server.py
+++ Python/Flask/Dojo_Fruit_Store/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import datetime
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -8,7 +9,29 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    if request.method == 'POST':
+        data = request.form
+        first_name = data['first_name']
+        last_name = data['last_name']
+        # How to get value from the form data as an specific type
+        # student_id = data.get('student_id', type=int)
+        student_id = data['student_id']
+
+        # Could do this "better" using a loop. As the list of items expands then that would be necessary or it'll be too much work
+        items = {}
+        items['Strawberry'] = data['strawberry']
+        items['Raspberry'] = data['raspberry']
+        items['Apple'] = data['apple']
+        items['Blackberry'] = data['blackberry']
+
+        # Get the total amount of ordered items
+        order_total = 0
+        for val in items.values():
+            order_total += int(val)
+        # Get the time the order was placed, as a friendly display string
+        order_time = datetime.datetime.now().strftime("%B %#d, %Y %H:%M:%S")
+
+    return render_template("checkout.html", first_name=first_name, last_name=last_name, student_id=student_id, items=items, order_total=order_total, order_time=order_time)
 
 @app.route('/fruits')         
 def fruits():
