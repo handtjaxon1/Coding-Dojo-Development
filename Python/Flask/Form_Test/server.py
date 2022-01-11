aa14d0617919dbd4b71b8993b5d25ae687d46efa
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-10 18:28:18 -0600

    Implemented sessions in the form test assignment

--- Python/Flask/Form_Test/server.py
+++ Python/Flask/Form_Test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "SuperSecretKey"
 
 @app.route('/')
 def root():
@@ -9,7 +10,13 @@ def root():
 def create_user():
     print("Received Post Info")
     print(request.form)
-    return redirect('/')
+    session['username'] = request.form['username']
+    session['useremail'] = request.form['useremail']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template("show.html")
 
 # only run if executing this file specifically
 if __name__=="__main__":
