9014ff0c4b95987880c20baa608e8353dd00c5d2
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-01-10 20:38:22 -0600

    Removed my unused JS from the counter assignment

--- Python/Flask/Counter/static/js/script.js
+++
@@ -1,6 +0,0 @@
-function submitForm() {
-    console.log("Hello world")
-    document.getElementById("increment-submit").submit();
-}
-
-document.getElementById("increment-submit").addEventListener("click", function(){ submitForm(); })
