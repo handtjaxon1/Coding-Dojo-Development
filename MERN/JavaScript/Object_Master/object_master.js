a9e3d3249b44a013c7cf17c731af736ae606da95
Jaxon Desktop <handtjaxon@gmail.com>
2022-02-22 12:24:34 -0600

    Implemented all filters/map tests for the pokemon object assignment

--- MERN/JavaScript/Object_Master/object_master.js
+++ MERN/JavaScript/Object_Master/object_master.js
@@ -24,14 +24,42 @@ const pokemon = Object.freeze([
     { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
 ]);
 
+// An array of pokemn that have names starting with the letter "B"
 const bListPokemon = pokemon.filter(p => p.name[0] === "B");
 console.log(bListPokemon);
 
+// An array of just the pokemon ids
 const pokemonIds = pokemon.map(p => p.id);
 console.log(pokemonIds);
 
+// An array of pokemon where the id is evenly divisible by 3
 const pkmnIdsDivByThree = pokemon.filter(p => p.id % 3 === 0);
 console.log(pkmnIdsDivByThree);
 
+// An array of pokemon objects that are "fire" type
 const firePokemon = pokemon.filter(p => p.types.find(type => type === "fire"));
 console.log(firePokemon);
+
+// An array of pokemon objects that have more than one type
+const multTypePokemon = pokemon.filter(p => p.types.length > 1);
+console.log(multTypePokemon);
+
+// An array with just the names of the pokemon
+const pokemonNames = pokemon.map(p => p.name);
+console.log(pokemonNames);
+
+// An array with just the names of pokemon that have an id greater than 99
+const pokemonHighId = pokemon.filter(p => p.id > 99).map(p => p.name);
+console.log(pokemonHighId);
+
+// An array with just the names of pokemon whose only type is "poison"
+const onlyPoisonPokemon = pokemon.filter(p => p.types.length === 1 && p.types.find(type => type === "poison")).map(p => p.name);
+console.log(onlyPoisonPokemon);
+
+// An array containing just the first type of all pokemon whose second type is "flying"
+const firstTypeFlyingPokemon = pokemon.filter(p => p.types.length > 1 && p.types[1] === "flying").map(p => p.types[0]);
+console.log(firstTypeFlyingPokemon);
+
+// A count of the number of pokemon that are "normal" type
+const numNormalPokemon = pokemon.filter(p => p.types.find(type => type === "normal")).length;
+console.log("Number of Normal Type Pokemon: ", numNormalPokemon);
