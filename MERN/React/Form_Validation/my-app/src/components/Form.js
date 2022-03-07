d935739f26b4168486e90d7692aba979ff4f2f48
Jaxon Handt - Laptop <handtjaxon@gmail.com>
2022-03-07 17:21:41 -0600

    Updated react form validation with form inputs

--- MERN/React/Form_Validation/my-app/src/components/Form.js
+++ MERN/React/Form_Validation/my-app/src/components/Form.js
@@ -23,12 +23,39 @@ function reducer(state, action) {
     };
 }
 
+function validateEmail(emailInput) {
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailInput.value.match(emailFormat);
+}
+
 function Form(props) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        dispatch({
+            type: name,
+            payload: value
+        });
+    }
+
     return (
         <div>
-            <form className="formDiv">
+            <form className="formDiv" action="#" method="post">
                 <div className="inputDiv">
-                    <label for="firstName">First Name</label>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" name="firstName" />
+                </div>
+                <div className="inputDiv">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" />
+                </div>
+                <div className="inputDiv">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div className="inputDiv">
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         </div>
