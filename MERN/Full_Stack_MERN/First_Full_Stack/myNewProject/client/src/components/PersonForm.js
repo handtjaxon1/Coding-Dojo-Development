16b183cbf88238c01cb7cda1e1e003a9341a05c1
Jaxon Desktop <handtjaxon@gmail.com>
2022-03-29 22:10:47 -0500

    Updated the client side to include a form for adding new 'Person' to the DB

--- MERN/Full_Stack_MERN/First_Full_Stack/myNewProject/client/src/components/PersonForm.js
+++ MERN/Full_Stack_MERN/First_Full_Stack/myNewProject/client/src/components/PersonForm.js
@@ -2,18 +2,50 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function PersonForm() {
-    const [message, setMessage] = useState("Loading...");
+    // Code used for the default Hello World testing
+    // const [message, setMessage] = useState("Loading...");
 
-    useEffect(() => {
-        axios.get("http://localhost:8000/api")
-            .then(res => setMessage(res.data.message))
-            .catch(err => console.log(err))
-    }, []);
+    // useEffect(() => {
+    //     axios.get("http://localhost:8000/api")
+    //         .then(res => setMessage(res.data.message))
+    //         .catch(err => console.log(err))
+    // }, []);
+
+    // Kepp track of what is being type via the useState hook
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    // Handle when the form is submitted
+    function handleOnSubmit(e) {
+        // Prevent default form behavior, ie refreshing the page on submit
+        e.preventDefault();
+        // Make a post request to creat a new person
+        axios.post("http://localhost:8000/api/people", {
+            firstName, // This is shorthand syntax for firstName: firstName
+            lastName
+        })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
+    }
 
     return (
-        <div>
-            <h2>Message from the backend: {message}</h2>
-        </div>
+        <form onSubmit={handleOnSubmit}>
+            <p>
+                <label htmlFor="firstName">First Name</label>
+                <br/>
+                {/* When the user types in this input, our onChange synthetic event runs this arrow function, setting that event's target's (input) value (what's typed into the input) to our updated state. You could call another function here instead of using the arrow function FYI. */}
+                <input type="text" id="firstName" onChange={(e) => setFirstName(e.target.value)} />
+            </p>
+            <p>
+                <label htmlFor="lastName">Last Name</label>
+                <br/>
+                <input type="text" id="lastName" onChange={(e) => setLastName(e.target.value)} />
+            </p>
+            <input type="submit"/>
+        </form>
     );
 }
 
