0a9516e58690de6926036380c597d81401f7aae4
Jaxon Desktop <handtjaxon@gmail.com>
2022-04-01 17:20:59 -0500

    Added the BankAccount OOP assignment

---
+++ MERN/OOP/BankAccount/BankAccount.js
@@ -0,0 +1,39 @@
+class BankAccount {
+    constructor(intRate, balance) {
+        this.intRate = intRate;
+        this.balance = this.balance;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        return this;
+    }
+
+    withdraw(amount) {
+        if (this.balance - amount >= 0) {
+            this.balance -= amount;
+        } else {
+            print("Insufficient funds: Charge a $5 fee");
+            this.balance -= 5;
+        }
+        return this;
+    }
+
+    displayAccountInfo() {
+        print("Balance: $" + this.balance);
+        return this;
+    }
+
+    yieldInterest() {
+        if (this.balance > 0) {
+            this.balance += (this.balance * this.intRate);
+        }
+        return this;
+    }
+}
+
+const jaxonBankAcct = new BankAccount(0.5, 1000);
+const alecBankAcct = new BankAccount(0.125, 250);
+
+jaxonBankAcct.deposit(50).deposit(75).deposit(100).withdraw(600).yieldInterest().displayAccountInfo();
+alecBankAcct.deposit(250).deposit(250).withdraw(50).withdraw(40).withdraw(30).withdraw(20).yieldInterest().displayAccountInfo();
