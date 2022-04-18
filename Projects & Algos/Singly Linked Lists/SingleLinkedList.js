7179fdb9d9f07c56f280fd679bd623da7a86d274
Jaxon Desktop <handtjaxon@gmail.com>
2022-04-18 10:21:12 -0500

    Changed name of SLL file, and added 'Contains' assignment

--- Projects & Algos/Singly Linked Lists/SingleLinkedList.js
+++ Projects & Algos/Singly Linked Lists/SingleLinkedList.js
@@ -9,7 +9,8 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-
+    
+    //------------------------------------ Fronts Assignment ------------------------------------//
     // Add front
     addFront(val) {
         // Create a new node
@@ -47,4 +48,26 @@ class LinkedList {
         }
         return this.head.data;
     }
+
+    //------------------------------------ Contains Assignment ------------------------------------//
+    contains(val) {
+        if (!this.head) {
+            return this;
+        }
+
+        // Travers the list from the front to back
+        let runner = this.head;
+        while (runner !== null) {
+            // If the current node's value matches then return true
+            if (runner.data === val) {
+                return true;
+            }
+            // ...otherwise continue to the next node
+            runner = runner.next;
+        }
+        // If we reach this point, then the list didn't have the value in any node
+        return false;
+    }
+
+    
 }
