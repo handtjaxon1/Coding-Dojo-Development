2cad64344e11677bea2e3194b28dea42662fe11f
Jaxon Desktop <handtjaxon@gmail.com>
2022-04-18 11:04:00 -0500

    Pushing previously unsaved changes

--- Projects & Algos/Arrays/Arrays_I/Arrays_Todo_02.js
+++ Projects & Algos/Arrays/Arrays_I/Arrays_Todo_02.js
@@ -1,16 +1,41 @@
 // Reverse
 function reverse(arr) {
+    // Since we're doing this in place, without another array, we can look at only looping through half of the array
+    // and essentially just taking the elements from the back half and swapping them with the front half.
+    for (let index = 0; index < arr.length / 2; index++) {
+        // Store the current value
+        let temp = arr[index];
+        // Set the current value to it's reversed counterpart index, and vice versa
+        let revInd = arr.length - 1 - index; // Just here to make it more clear what this value means. Not strictly neccessary
+        arr[index] = arr[revInd];
+        arr[revInd] = temp;
+    }
     return arr;
 }
 
 // Rotate
 function rotate(arr, shiftBy) {
+    // This one took me far too long to solve lol
     return arr;
 }
 
 // Filter Range
 function filterRange(arr, min, max) {
-    
+    // We can track the next index that has a value between min and max
+    // so that we don't have to iterate through the array twice
+    let next = 0;
+    for (let index = 0; index < arr.length; index++) {
+        // Check if the value of the current index is within min and max
+        if (arr[index] >= min && arr[index] <= max) {
+            //...if so then we can set the array element of our 'next' to the current value
+            // and increase next. This allows us to overwrite values of the array from the front
+            // so that we can simply remove the excess elements once the loop is done (see below)
+            arr[next] = arr[index];
+            next++;
+        }
+    }
+    // Then we can remove the extra elements by simply setting the length of the array directly; no need to pop anything
+    arr.length = next;
     return arr;
 }
 
